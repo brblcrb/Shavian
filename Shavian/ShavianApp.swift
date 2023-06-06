e55adcf7a180a625cbea2082c3473ee451d3a5cc
Albert <albert11bc@gmail.com>
2023-06-06 08:45:10 +0200

    Initial Commit

---
+++ Shavian/ShavianApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShavianApp.swift
+//  Shavian
+//
+//  Created by Albert on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ShavianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
